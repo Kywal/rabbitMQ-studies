61151edfd1b60cae7a22233b4b86b9a4b16655ec
Emanuel Kywal <emanuelkywal@outlook.com>
2025-06-11 15:49:36 -0300

    feat(basic-messaging): project structure

---
+++ basic-messaging-with-RabbitMQ/src/test/java/com/example/basicmessagingwithrabbitmq/BasicMessagingWithRabbitMQTests.java
@@ -0,0 +1,13 @@
+package com.example.basicmessagingwithrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicMessagingWithRabbitMQTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
