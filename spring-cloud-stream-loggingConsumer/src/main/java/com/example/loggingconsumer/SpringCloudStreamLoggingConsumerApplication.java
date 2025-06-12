951d040900f12587a19ca4c608bea4af1597ac16
Emanuel Kywal <emanuelkywal@outlook.com>
2025-06-12 14:24:29 -0300

    feat(loggingConsumer): basic person consumer

--- spring-cloud-stream-loggingConsumer/src/main/java/com/example/loggingconsumer/SpringCloudStreamLoggingConsumerApplication.java
+++ spring-cloud-stream-loggingConsumer/src/main/java/com/example/loggingconsumer/SpringCloudStreamLoggingConsumerApplication.java
@@ -1,7 +1,12 @@
 package com.example.loggingconsumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class SpringCloudStreamLoggingConsumerApplication {
@@ -10,4 +15,42 @@ public class SpringCloudStreamLoggingConsumerApplication {
         SpringApplication.run(SpringCloudStreamLoggingConsumerApplication.class, args);
     }
 
+    private static final Logger log = LoggerFactory.getLogger(SpringCloudStreamLoggingConsumerApplication.class);
+
+    @Bean
+    public Consumer<Person> log() {
+        return person -> {
+            // Log the received Person object
+            log.info("Received Person: {}", person);
+        };
+    }
+    public static class Person {
+
+        private String name;
+        private int age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
 }
