a8561f80f3c190e56b85deba470a8668c5a78e79
Emanuel Kywal <emanuelkywal@outlook.com>
2025-06-12 12:10:11 -0300

    feat(loggingConsumer): initial project structure

---
+++ spring-cloud-stream-loggingConsumer/src/main/java/com/example/loggingconsumer/SpringCloudStreamLoggingConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.loggingconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamLoggingConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamLoggingConsumerApplication.class, args);
+    }
+
+}
